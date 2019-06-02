9871b5d65d68104760a2d0fe2cec55026a883b0a
Angel de la Torre <angel.dlt.1996@gmail.com>
2019-06-01 18:56:02 -0700

    Adding Test Cases

--- app/src/test/java/com/tianatonnu/handymaps/ExampleUnitTest.java
+++ app/src/test/java/com/tianatonnu/handymaps/ExampleUnitTest.java
@@ -10,6 +10,17 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+
+    private Building testBuilding = new Building(new double[]{-120.653077, 35.297469},"Yosemite Hall Tower 8","114J");
+
+    @Test
+    public void buildingTest(){
+        assertEquals(35.297469,testBuilding.getLatitude(),0.1);
+        assertEquals(-120.653077,testBuilding.getLongitude(),0.1);
+        assertEquals("Yosemite Hall Tower 8",testBuilding.getBuildingName());
+        assertEquals("114J",testBuilding.getBuildingNumber());
+    }
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
