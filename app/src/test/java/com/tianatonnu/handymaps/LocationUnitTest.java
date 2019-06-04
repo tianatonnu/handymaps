4775f8a5a5ec3ab4b78e661eb33dd4bbf12f4804
Tatjana Ellis <teellis@calpoly.edu>
2019-06-04 16:01:53 -0700

    Added outlines for UnitTest classes.

---
+++ app/src/test/java/com/tianatonnu/handymaps/LocationUnitTest.java
@@ -0,0 +1,12 @@
+package com.tianatonnu.handymaps;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class LocationUnitTest {
+    // Test createCard() for
+
+    // - Course class
+
+    // - Classroom class
+}
