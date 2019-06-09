7c55e12bccb8bd4118f401f2007447cc101d381a
Tatjana Ellis <teellis@calpoly.edu>
2019-06-09 04:05:06 -0700

    Description of test cases, not implemented yet.

--- app/src/test/java/com/tianatonnu/handymaps/ScheduleUnitTest.java
+++ app/src/test/java/com/tianatonnu/handymaps/ScheduleUnitTest.java
@@ -7,14 +7,78 @@ public class ScheduleUnitTest {
 
     private Schedule schedule = new Schedule();
 
-    // addCourse() tests
+    // - - - - - - - - - - - - - - - - - - - -
+    //         - addCourse() tests -
+    // - - - - - - - - - - - - - - - - - - - -
+    // 1. Adds classes correctly
+    @Test
+    public void addNewClassesShouldAdd(){
 
-    // removeCourse() tests
+    }
 
-    // deleteSchedule() tests
+    // 2. Does not add duplicate classes
+    @Test
+    public void addingDuplicateClassShouldNotAdd() {
 
-    // contains() tests
+    }
 
-    // Comparator tests
+    // - - - - - - - - - - - - - - - - - - - -
+    //       - removeCourse() tests -
+    // - - - - - - - - - - - - - - - - - - - -
+    // 1. Does not remove from empty schedule
+    @Test
+    public void removeFromEmptyScheduleShouldNotRemove() {
+
+    }
+
+    // 2. Removes correctly when present
+    @Test
+    public void removePresentClassShouldRemove() {
+
+    }
+
+    // 3. Does not remove if not present
+    @Test
+    public void removeClassNotPresentShouldNotRemove() {
+
+    }
+
+
+    // - - - - - - - - - - - - - - - - - - - -
+    //       - deleteSchedule() tests -
+    // - - - - - - - - - - - - - - - - - - - -
+    // 1. Check all courses removed from schedule
+    @Test
+    public void deleteScheduleShouldEmptySchedule() {
+
+    }
+
+
+    // - - - - - - - - - - - - - - - - - - - -
+    //         - contains() tests -
+    // - - - - - - - - - - - - - - - - - - - -
+    // 1. Returns FALSE if schedule is empty
+    @Test
+    public void containsEmptyScheduleShouldReturnFalse() {
+
+    }
+
+    // 2. Returns TRUE if it contains class
+    @Test
+    public void containsPresentClassShouldReturnTrue() {
+
+    }
+
+    // 3. Returns FALSE if it does not contain class
+    @Test
+    public void containsClassNotPresentShouldReturnFalse() {
+
+    }
+
+
+    // - - - - - - - - - - - - - - - - - - - -
+    //     - Schedule comparator tests -
+    // - - - - - - - - - - - - - - - - - - - -
+
 
 }
